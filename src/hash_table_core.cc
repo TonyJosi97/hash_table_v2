cf695919dd054f75586af6291c42b8b465c1018c
Tony Josi <tonyjosinew@gmail.com>
2020-08-18 00:04:08 +0530

    fix with file name

--- src/hash_table_core.cc
+++ src/hash_table_core.cc
@@ -1,3 +1,15 @@
+/** 
+ *  @file   hash_table_core.c
+ *  @brief  Library function defintions
+ *
+ *  This contains the Library function defintions
+ *
+ *  @author         Tony Josi   https://tonyjosi97.github.io/profile/
+ *  @copyright      Copyright (C) 2020 Tony Josi
+ *  @bug            No known bugs.
+ */
+
+
 #include "../inc/hash_table_v2.hpp"
 
 ht_v2::hash_table::hash_table(
