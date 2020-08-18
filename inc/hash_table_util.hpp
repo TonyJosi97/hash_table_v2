efff35c9123230bdf6fe9844f1819e40f28a5437
Tony Josi <tonyjosinew@gmail.com>
2020-08-19 00:31:07 +0530

    util func header added

---
+++ inc/hash_table_util.hpp
@@ -0,0 +1,17 @@
+/** 
+ *  @file   hash_table_util.hpp
+ *  @brief  Library function defintions
+ *
+ *  This contains helper function defintions
+ *
+ *  @author         Tony Josi   https://tonyjosi97.github.io/profile/
+ *  @copyright      Copyright (C) 2020 Tony Josi
+ *  @bug            No known bugs.
+ */
+
+#include "hash_table_v2.hpp"
+
+unsigned long    __ght_core_util_get_hash(const unsigned long, const unsigned long, unsigned int);
+unsigned long    get_time_in_nanosec(void);
+unsigned int     get_next_prime(unsigned int);
+
